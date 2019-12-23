05ebdef98fc6cb235e0d36720a0652b760c36f56
Artem Oliinyk <55375811+ArtOliinyk@users.noreply.github.com>
2019-12-23 22:03:02 +0200

    Update task3 in cw2

--- oliinykao/second/task3.py
+++ oliinykao/second/task3.py
@@ -2,6 +2,8 @@
 Вивести даний текст по одному слову, щоб остання літера була у верхньому регістрі
 """
 mytext = "London is the capitol of Great Britain"
-mylist = mytext.split(' ')
-for word in mylist:
-    print(word[:len(word)-1] + word[len(word) - 1].upper())
+def last_letter_upper(string):
+    list_of_words = re.split("[ .,!?:;-]", string)
+    for word in list_of_words:
+        print(word[:len(word)-1] + word[len(word) - 1].upper())
+last_letter_upper(mytext)
