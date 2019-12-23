fa6fa39c7a46d7a31a53fc5b623d91188e12f883
Artem Oliinyk <55375811+ArtOliinyk@users.noreply.github.com>
2019-12-23 21:59:12 +0200

    Update control work 1

--- oliinykao/first/task1.py
+++ oliinykao/first/task1.py
@@ -3,21 +3,16 @@
 Вивести YES якщо конструкція begin-end є правильною у тексті.
 (Всередині конструкції може бутиле лише одна ще така)
 """
-import re
-import string
-
 def check_text():
     text = input("Введіть строку: ")
-    elements = []
-    elements = text.split(text)
     begins = 0
     ends = 0
-    for element in elements:
-        if element == "begin":
-            begins = begins + 1
-        if element == "ends":
-            ends = ends + 1
-    if begins == ends & bool(re.match("^begin")):
+    for element in text:
+        if element == "(":
+            begins += 1
+        if element == ")":
+            ends += 1
+    if (begins == ends) & (begins != 0) & (ends != 0):
         return "YES"
     else:
         return "NO"
